47638f88aa1d995aa4cce8a0ed4c80974c27e83c
Benatovich <sambenatovich@gmail.com>
2021-12-13 15:51:59 -0500

    i think fetchDogs and fetchDog(id) are written properly

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { getDogs } from './fetch-utils.js';
+import { fetchDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
@@ -6,3 +6,12 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // fetch all dogs
 // render and append all dog cards to the container
+
+window.addEventListener('load', async() => {
+    const dogs = await fetchDogs();
+    for (let dog of dogs) {
+        const dogEl = renderDogCard(dog);
+        // render dog deetz
+        dogListContainer.append(dogEl);
+    }
+});
